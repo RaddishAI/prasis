3c93fd1a25151ac04cad62310683dbdc81732daa
RaddishAI <petter.arbeid@gmail.com>
2025-09-25 14:21:52 +0200

    test

---
+++ scripts/constants.js
@@ -0,0 +1,20 @@
+/**
+ * !!!
+ * Base URL
+ * !!!
+ */
+
+/**
+ * Base-URL for all API calls
+ *
+ * Ex: '${BASE_API_URL}/products'
+ *
+ * @constant {string}
+ */
+const BASE_API_URL = "https://vs.api.noroff.dev";
+
+/**
+ * !!!
+ * Auth endpooints
+ * !!!
+ */
---
+++ utils/config.js
@@ -0,0 +1,4 @@
+export const BASE_URL = "https://v2.api.noroff.dev";
+export const ACCESS_TOKEN_KEY = "accessToken";
+export const API_KEY_STORAGE = "x-Noroff-API-Key";
+export const API_KEY_NAME = "prasis-local-dev";
